9f7d5586e5686aaf2faf3608d24b9fa413cb873c
Pranit Kanase <kanasepranit2@gmail.com>
2021-05-12 19:19:23 +0530

    s

--- springrest/src/main/java/com/springrest/springrest/services/CourseService.java
+++ springrest/src/main/java/com/springrest/springrest/services/CourseService.java
@@ -9,4 +9,12 @@ import com.springrest.springrest.entities.Course;
 public interface CourseService {
 	
 	public List<Course> getCourses();
+	
+	public Course getCourse(long courseId);
+	
+	public Course addCourse(Course course);
+	
+	public Course updateCourse(Course course);
+	
+	public void deleteCourse(long parseLong);
 }
--- springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -3,28 +3,83 @@ package com.springrest.springrest.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
-	List<Course> list;
+	//List<Course> list;
+	
+	@Autowired
+	private CourseDao courseDao;
 	
 	public CourseServiceImpl() {
 		
-		list=new ArrayList();
-		list.add(new Course(14,"Java","Core Java"));
-		list.add(new Course(15,"Springboot","J2EE"));
-		list.add(new Course(16,"REST API","J2EE"));
+		/*
+		 * list=new ArrayList(); list.add(new Course(14,"Java","Core Java"));
+		 * list.add(new Course(15,"Springboot","J2EE")); list.add(new
+		 * Course(16,"REST API","J2EE"));
+		 */
 	}
 	
 
 	@Override
 	public List<Course> getCourses() {
-		return list;
+		/* return list; */
+		return courseDao.findAll();
+	}
+
+
+	@Override
+	public Course getCourse(long courseId) {
+		/*
+		 * Course c = null; for(Course course : list) { if(course.getId() == courseId) {
+		 * c= course; break; } } return c;
+		 */
+		
+		return courseDao.getOne(courseId);
+	}
+
+
+	@Override
+	public Course addCourse(Course course) {
+		/*
+		 * list.add(course); return course;
+		 */
+		
+		courseDao.save(course);
+		return course;
+	}
+
+
+	@Override
+	public Course updateCourse(Course course) {
+		/*
+		 * list.forEach(e -> { if(e.getId() == course.getId()) {
+		 * e.setTitle(course.getTitle()); e.setDescription(course.getDescription()); }
+		 * }); return null;
+		 */
+		
+		courseDao.save(course);
+		return course;
+
+	}
+
+
+	@Override
+	public void deleteCourse(long parseLong) {
+		/*
+		 * list.forEach(); return null;
+		 */
+		Course entity = courseDao.getOne(parseLong);
+		courseDao.delete(entity);
+		
+		
 	}
 	
 }
